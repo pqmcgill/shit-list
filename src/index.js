2df9ce6b655466535d0c3a33723f024e6472bd80
pqmcgill <pqmcgill@gmail.com>
2019-07-13 15:34:59 -0400

    moved POC code out of app

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom'
 import * as serviceWorker from './serviceWorker';
 import App from './App'
 
+// TODO: remove eventually.
+import './poc-tmp'
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
