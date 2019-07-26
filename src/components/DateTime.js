f88a461f36aca2443a8732be77f1525379f491a9
Jason Tuttle <jason.tuttle@greenlight.guru>
2019-07-26 12:41:02 -0400

    add components

---
+++ src/components/DateTime.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import moment from 'moment';
+import styled from 'styled-components';
+
+const DateWrapper = styled.div`
+  display: inline-block;
+  font-size: 1rem;
+`
+
+const DateTime = function(props) {
+  const {date} = props;
+  return (
+    <DateWrapper>
+      {moment(date).format('dddd, MMMM Do YYYY, h:mm:ss a')}
+    </DateWrapper>
+  )
+}
+
+export default DateTime;
---
+++ src/components/FormDiv.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const FormDiv = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+`
+
+export default FormDiv;
